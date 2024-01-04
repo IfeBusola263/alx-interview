992520cfa160028ddb7a0021e9e934f229fe9f97
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-04 22:18:17 +0000

    import chanllenge

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,14 +2,34 @@
 """
 This implements the Pascal's triangle.
 """
-from math import factorial
 
+# def pascal_triangle(n):
+#     """
+#     The Pascal's triangle implementation.
+#     """
+
+#     if n <= 0:
+#         return []
+
+#     outerArr = []
+
+#     for i in range(n):
+#         innerArr = []
+#         for r in range(i + 1):
+#             ncr = factorial(i) // (factorial(r) * factorial(i-r))
+#             innerArr.append(ncr)
+#         outerArr.append(innerArr)
+#     return outerArr
+
+def calculate_factorial(num):
+    """Calculate the factorial of a number."""
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+    return result
 
 def pascal_triangle(n):
-    """
-    The Pascal's triangle implementation.
-    """
-
+    """Generate Pascal's triangle up to the nth row."""
     if n <= 0:
         return []
 
@@ -18,7 +38,9 @@ def pascal_triangle(n):
     for i in range(n):
         innerArr = []
         for r in range(i + 1):
-            ncr = factorial(i) // (factorial(r) * factorial(i-r))
+            ncr = calculate_factorial(i) // (
+                calculate_factorial(r) * calculate_factorial(i - r))
             innerArr.append(ncr)
         outerArr.append(innerArr)
+
     return outerArr
