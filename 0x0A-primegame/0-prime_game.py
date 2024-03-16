9c8cced7664d0b4ad6f8801f7357ad80fef48119
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-03-16 03:50:44 +0100

    handled cases with no number of rounds

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,33 +7,35 @@ Main Module for Prime game.
 def isWinner(x, nums):
     """ Determine the winner of each game."""
 
-    def is_prime(num):
-        """Check if a number is prime."""
-        if num <= 1:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
+    if x > 0:
+        def is_prime(num):
+            """Check if a number is prime."""
+            if num <= 1:
                 return False
-        return True
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    return False
+                return True
 
-    # Determine the winner for each round
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        if n <= 1:
-            # If n is 0 or 1, there are no prime numbers, so Ben wins
-            ben_wins += 1
-        else:
-            prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-            if prime_count % 2 == 0:
+        # Determine the winner for each round
+        maria_wins = 0
+        ben_wins = 0
+        for n in nums:
+            if n <= 1:
+                # If n is 0 or 1, there are no prime numbers, so Ben wins
                 ben_wins += 1
             else:
-                maria_wins += 1
+                prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+                if prime_count % 2 == 0:
+                    ben_wins += 1
+                else:
+                    maria_wins += 1
 
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+        # Determine the overall winner
+        if maria_wins > ben_wins:
+            return "Maria"
+        elif ben_wins > maria_wins:
+            return "Ben"
+        else:
+            return None
+    return None
