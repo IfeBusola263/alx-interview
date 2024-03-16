80355bca3934dfc5f3016d251331d51513be9867
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-03-16 03:56:36 +0100

    repositioned the return in the isprime method

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -15,7 +15,7 @@ def isWinner(x, nums):
             for i in range(2, int(num**0.5) + 1):
                 if num % i == 0:
                     return False
-                return True
+            return True
 
         # Determine the winner for each round
         maria_wins = 0
